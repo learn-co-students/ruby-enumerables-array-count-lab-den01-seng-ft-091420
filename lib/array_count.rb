5ae79887a823b0400467ca87eedcf5b605100584
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-04 05:24:40 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,18 @@
-def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+require 'pry'
+array = [0, 1, "", 24, 5, -10]
+
+array2 = [3, "hey", -9, 24, "string here", -10]
+
+
+def count_strings(array2)
+  array2.count do |element|
+  element.class == String
+  end
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count do |element|
+  element.to_s == ""
+  end  
 end
+
